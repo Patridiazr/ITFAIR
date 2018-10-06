b9052a85c3a3853462dc246d7ce38c22b28088e8
Patricio <patri.diazr@alumnos.duoc.cl>
2018-10-05 23:16:15 -0300

    pdf y cambios en tarjetas , footer

--- js/app.js
+++ js/app.js
@@ -1,28 +1,13 @@
-//Modal 1 //
-// Get the modal
-var modal = document.getElementById('myModal');
-
-// Get the button that opens the modal
-var btn = document.getElementById("myBtn");
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks on the button, open the modal 
-btn.onclick = function() {
-    modal.style.display = "block";
-}
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-    modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
-}   
-
-
+if ( ! Modernizr.adownload ) {
+    var $link = $('a');
+   
+      $link.each(function() {
+          var $download = $(this).attr('download');
+       
+          if (typeof $download !== typeof undefined && $download !== false) {
+        var $el = $('<div>').addClass('download-instruction').text('Right-click and select "Download Linked File"');
+        $el.insertAfter($(this));
+          }
+   
+      });
+  }
