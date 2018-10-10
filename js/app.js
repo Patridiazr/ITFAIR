d12d6f87211320e64089183e8e46741c9d604580
Jose Guerra <nataxos@gmail.com>
2018-10-10 20:07:57 -0300

    Validaciones para SingUp, quedo la caga, hagan algooooo

--- js/app.js
+++ js/app.js
@@ -12,7 +12,7 @@ if ( ! Modernizr.adownload ) {
       });
 }
 
-function validarFormulario(){
+function valContacUs(){
     
     var nombre, correo, telefono, organizacion, mensaje
     nombre = document.getElementById("nom").value
@@ -24,8 +24,7 @@ function validarFormulario(){
     if(nombre == "" || correo == "" || mensaje == ""){
         alert("the fields name, mail and message are obligatory")
         return false
-    }
-    
+    }    
     
     if(telefono <10000000 || telefono >99999999){    
         alert("The cell phone number needs 8 digits.")
@@ -34,6 +33,37 @@ function validarFormulario(){
 
     if(mensaje.lengt > 200){
         alert("Max length of the message is 200 characters")
+        return false
     }
     
 }
+function valSingUp(){
+    var nombre, nacionalidad, correo, contrasena, contrasena2
+    nombre = document.getElementById("nom") .value
+    nacionalidad = document.getElementById("nac").value
+    correo = document.getElementById("ema").value
+    contrasena = document.getElementById("pas").value
+    contrasena2 = document.getElementById("pas2").value
+
+    if(nombre == "" || correo == "" || contrasena == "" || nacionalidad == ""){
+        alert("All the fields are obligatory")
+        return false
+    }
+
+    if(contrasena != contrasena2){
+        alert("Passwords do not match")
+        return false
+    }
+
+}
+
+function valSelect(obj){
+    var value
+    value = obj.value    
+    console.log(value)
+    if(value == 3 || value == "3" || value == "d"){
+             
+        document.getElementById("nac").prop.disabled = false 
+    }
+}
+
